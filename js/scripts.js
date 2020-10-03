def76b4a5b1bd20d1f6c516245e53e6586a83748
Ornella-KK <ornellakankazi@gmail.com>
2020-10-03 14:59:44 +0200

    added jquery for fourth section

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// jquery for paragraphs in what we do icons
+$(document).ready(function(){
+   $("img#first").click(function(){
+       $("#para1").toggle();
+   })
+   $("img#second").click(function(){
+       $("#para2").toggle();
+   })
+   $("img#third").click(function(){
+       $("#para3").toggle();
+   })
+})
