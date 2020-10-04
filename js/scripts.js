15e5f72889a090828c67633fc46a1e53c90d1392
Ornella-KK <ornellakankazi@gmail.com>
2020-10-04 19:26:10 +0200

    added more functions on what to do section

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,28 @@
 // jquery for paragraphs in what we do icons
 $(document).ready(function(){
    $("img#first").click(function(){
-       $("#para1").toggle();
+        $("#para1").toggle()
+        $("img#first").toggle();
+   })
+   $("#para1").click(function(){
+       $("img#first").show();
+       $("#para1").hide();
    })
    $("img#second").click(function(){
        $("#para2").toggle();
+       $("img#second").toggle();
+   })
+   $("#para2").click(function(){
+       $("img#second").show();
+       $("#para2").hide();
    })
    $("img#third").click(function(){
        $("#para3").toggle();
+       $("img#third").toggle();
+   })
+   $("#para3").click(function(){
+       $("img#third").show();
+       $("#para3").hide();
    })
 })
 //  alerting when submitting contact info
